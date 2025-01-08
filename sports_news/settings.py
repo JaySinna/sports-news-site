40d3d51a2103fb1fae6ffb3c4c81d899b8a41f70
Jay SinnaduraI <jay.sinnadurai@gmail.com>
2025-01-08 20:19:01 +0000

    Create secret key and add to settings

--- sports_news/settings.py
+++ sports_news/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-52m4$9p$vl__4rptg_6*!)mi7v(+hwhprf^%wf666)44o^k-6v'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
