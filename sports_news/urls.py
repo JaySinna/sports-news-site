303360dd9f90c3916a8b52353cca69474b71a395
Jay SinnaduraI <jay.sinnadurai@gmail.com>
2025-01-12 09:57:13 +0000

    Replace blog url path

--- sports_news/urls.py
+++ sports_news/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('blog/', my_blog, name='blog'),
+    path("", include("blog.urls"), name="blog-urls"),
 ]
