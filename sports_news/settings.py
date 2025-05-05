e3232c9e63a7f89d172172f6fa270c69b782f7c1
Jay Sinnadurai <jay.sinnadurai@gmail.com>
2025-05-05 11:29:32 +0100

    Modified files for VS Code migration

--- sports_news/settings.py
+++ sports_news/settings.py
@@ -30,7 +30,12 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['8000-jaysinna-sportsnewssite-dmclnihvra9.ws.codeinstitute-ide.net', '.herokuapp.com']
+ALLOWED_HOSTS = [
+    '8000-jaysinna-sportsnewssite-dmclnihvra9.ws.codeinstitute-ide.net',
+    '.herokuapp.com',
+    '127.0.0.1',
+    'localhost'
+]
 
 
 # Application definition
